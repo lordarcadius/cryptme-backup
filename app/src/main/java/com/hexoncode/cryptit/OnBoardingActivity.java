01837835b9b7e9bcf81a7321a37704c189973747
Vipul Jha <vipuljha08@gmail.com>
2020-07-12 16:52:15 +0530

    OnBoarding: Add on boarding screen
    Signed-off-by: Vipul Jha <vipuljha08@gmail.com>

---
+++ app/src/main/java/com/hexoncode/cryptit/OnBoardingActivity.java
@@ -0,0 +1,50 @@
+package com.hexoncode.cryptit;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+
+import com.hololo.tutorial.library.Step;
+import com.hololo.tutorial.library.TutorialActivity;
+
+public class OnBoardingActivity extends TutorialActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addFragment(new Step.Builder().setTitle("Welcome")
+                .setContent("faucibus scelerisque eleifend donec pretium vulputate sapien nec sagittis aliquam malesuada bibendum arcu vitae elementum curabitur vitae nunc sed velit")
+                .setBackgroundColor(Color.parseColor("#232637")) // int background color
+                .setDrawable(R.drawable.ic_crypt_logo) // int top drawable
+                .build());
+
+        addFragment(new Step.Builder().setTitle("Title 1")
+                .setContent("faucibus scelerisque eleifend donec pretium vulputate sapien nec sagittis aliquam malesuada bibendum arcu vitae elementum curabitur vitae nunc sed velit")
+                .setBackgroundColor(Color.parseColor("#232637")) // int background color
+                .setDrawable(R.drawable.ic_crypt_logo) // int top drawable
+                .build());
+
+        addFragment(new Step.Builder().setTitle("Title 2")
+                .setContent("faucibus scelerisque eleifend donec pretium vulputate sapien nec sagittis aliquam malesuada bibendum arcu vitae elementum curabitur vitae nunc sed velit")
+                .setBackgroundColor(Color.parseColor("#232637")) // int background color
+                .setDrawable(R.drawable.ic_crypt_logo) // int top drawable
+                .build());
+
+        addFragment(new Step.Builder().setTitle("Title 2")
+                .setContent("faucibus scelerisque eleifend donec pretium vulputate sapien nec sagittis aliquam malesuada bibendum arcu vitae elementum curabitur vitae nunc sed velit")
+                .setBackgroundColor(Color.parseColor("#232637")) // int background color
+                .setDrawable(R.drawable.ic_crypt_logo) // int top drawable
+                .build());
+    }
+
+    @Override
+    public void finishTutorial() {
+        startActivity(new Intent(OnBoardingActivity.this, HomeActivity.class));
+        finish();
+    }
+
+    @Override
+    public void currentFragmentPosition(int position) {
+
+    }
+}
